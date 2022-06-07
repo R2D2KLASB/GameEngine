d10bd608081950177fcb7fc2d7939bdcc69632f7
Nic Jenneboer <nicjenneboer@gmail.com>
2022-06-07 16:18:56 +0200

    Play with ai or ai2ai

--- Engine/gameEngine.py
+++ Engine/gameEngine.py
@@ -11,24 +11,11 @@ class GameEngine():
     def setupPlayers(self):
         self.player1 = Player(self.row_size, self.col_size, 'player1')
 
-        self.nextPlayer('player2')
+        # if 'ai' not in self.player1.name and 'ai' not in self.player2.name:
+        #     self.nextPlayer('player2')
 
         print('Player 2 Setup Ships')
-        self.player2 = AIPlayer(self.row_size, self.col_size, 'player2')
-
-
-    def playerTurn(self, player, targetPlayer):
-        result = False
-        # NEED BETTER ERROR HANDLING
-        while result is False:
-            try:
-                result = player.Turn(targetPlayer)
-            except Exception as e:
-                clear()
-                print(e)
-                pass
-        clear()
-        print(player)
+        self.player2 = AIPlayer(self.row_size, self.col_size, 'ai2')
 
     def nextPlayer(self, name=False):
         input('\nNext?')
@@ -38,10 +25,15 @@ class GameEngine():
 
     def play(self):
         while not self.player1.checkDefeated():
-            self.nextPlayer(self.player1.name)
-            self.playerTurn(self.player1, self.player2)
+            if 'ai' not in self.player1.name and 'ai' not in self.player2.name:
+                self.nextPlayer(self.player1.name)
+            self.player1.Turn(self.player2)
             if self.player2.checkDefeated():
                 break
-            self.nextPlayer(self.player2.name)
-            self.playerTurn(self.player2, self.player1)
+            if 'ai' not in self.player1.name and 'ai' not in self.player2.name:
+                self.nextPlayer(self.player2.name)
+            self.player2.Turn(self.player1)
+        clear()
         print(('Player 1' if self.player2.checkDefeated() else 'Player2') + ' WON!')
+        print(self.player1 if self.player2.checkDefeated() else self.player2)
+
